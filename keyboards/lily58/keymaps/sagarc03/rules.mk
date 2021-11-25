1a186cf1cce54880a574c930b03bca2418cd4af8
Sagar Chavan <sagar.c.03@gmail.com>
2021-11-24 20:06:43 -0500

    add bootloader info to sagarc03

--- keyboards/lily58/keymaps/sagarc03/rules.mk
+++ keyboards/lily58/keymaps/sagarc03/rules.mk
@@ -13,6 +13,7 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= yes     # OLED display
+BOOTLOADER = qmk-dfu
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
